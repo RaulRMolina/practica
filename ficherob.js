6a59302405743f50bc417d9b5f65af8f6366493d
Raul <raulmolina3@gmail.com>
2024-11-26 11:58:20 +0100

    1

--- ficherob.js
+++ ficherob.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-console.log("*****************");
-console.log("soy el segundo fichero");
-console.log("*****************");
-=======
 console.log("-----------------");
 console.log("fichero II");
 console.log("-----------------");
->>>>>>> mirama
